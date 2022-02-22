c16fb15a9672fa7d29be37229bb5e9862d1a6194
chandana0818 <chandana.nagandla@kellogg.northwestern.edu>
2022-02-22 02:35:21 +0000

    Add solutions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,22 @@
 class CompaniesController < ApplicationController
+    def index
+        @companies = Company.all
+    end
+
+    def show
+        @company = Company.find(params[:id])
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def create
+        #@company = Company.new(params["company"])
+        @company = Company.new(params.require(:company).permit(:name, :city, :state))
+        @company.save
+        redirect_to "/companies"
+
+    end
+
 end
